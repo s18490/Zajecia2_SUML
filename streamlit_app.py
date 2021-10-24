bce22d999b419b1556437c43d9c45081b960b7cb
s18490 <80642753+s18490@users.noreply.github.com>
2021-10-24 12:49:24 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,8 +28,8 @@ if option == "Angielski -> Niemiecki":
         answer = classifier(text)
         my_bar = st.progress(0)
         for percent_complete in range(100):
-            time.sleep(0.2)
-            my_bar.progress(percent_complete +1)
+            time.sleep(0.1)
+            my_bar.progress(percent_complete +4)
         st.success("Super! Znaleziono tłumaczanie")
         st.write(answer)
 
